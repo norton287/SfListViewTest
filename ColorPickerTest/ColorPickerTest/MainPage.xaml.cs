507dc45ddac4497332faf5cd3eb00ac2386c370c
norton287 <42098364+norton287@users.noreply.github.com>
2019-11-09 07:53:46 -0600

    Add project files.

---
+++ ColorPickerTest/ColorPickerTest/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using ItemTappedEventArgs = Syncfusion.ListView.XForms.ItemTappedEventArgs;
+
+namespace SfListViewTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        private readonly MainPageViewModel _viewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _viewModel = new MainPageViewModel();
+
+            BindingContext = _viewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //Build picker list of color names and colors
+            _viewModel.BuildColorsList();
+        }
+
+        private void ListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (!(e.ItemData is Colors item)) return;
+
+            var index = listView.DataSource.DisplayItems.IndexOf(item);
+            
+            _viewModel.DoUpdate(item);
+
+            listView.LayoutManager.ScrollToRowIndex(index, Syncfusion.ListView.XForms.ScrollToPosition.Center, true);
+
+
+
+        }
+    }
+}
---
+++ ColorPickerTest/ColorPickerTest/MainPageViewModel.cs
@@ -0,0 +1,559 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace SfListViewTest
+{
+    public class MainPageViewModel
+    {
+        private readonly List<Colors> SubColors = new List<Colors>();
+
+        public ObservableCollection<Colors> ColorsObservableCollection { get; set; }
+
+        public MainPageViewModel()
+        {
+            ColorsObservableCollection = new ObservableCollection<Colors>();
+        }
+
+        public void DoUpdate(Colors item)
+        {
+            int index = ColorsObservableCollection.IndexOf(item);
+
+            ColorsObservableCollection.Remove(item);
+
+            item.BackGroundColor = "Test Color";
+            item.BgColor = Color.Black;
+
+            ColorsObservableCollection.Insert(index, item);
+        }
+
+        public void BuildColorsList()
+        {
+            ColorsObservableCollection?.Clear();
+
+            //Build colors observable collection
+            if (ColorsObservableCollection == null || ColorsObservableCollection.Count > 0) return;
+
+            //Build colors observable collection
+
+            SubColors.Clear();
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DodgerBlue",
+                ColorName = "Dodger Blue",
+                BgColor = Color.DodgerBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Tomato",
+                ColorName = "Tomato",
+                BgColor = Color.Tomato,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Green",
+                ColorName = "Green",
+                BgColor = Color.Green,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Orange",
+                ColorName = "Orange",
+                BgColor = Color.Orange,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Brown",
+                ColorName = "Brown",
+                BgColor = Color.Brown,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Maroon",
+                ColorName = "Maroon",
+                BgColor = Color.Maroon,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DarkSlateGray",
+                ColorName = "Dark Slate Gray",
+                BgColor = Color.DarkSlateGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DimGray",
+                ColorName = "Dim Gray",
+                BgColor = Color.DimGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightGreen",
+                ColorName = "Light Green",
+                BgColor = Color.LightGreen,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.CornflowerBlue",
+                ColorName = "Cornflower Blue",
+                BgColor = Color.CornflowerBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightSalmon",
+                ColorName = "Light Salmon",
+                BgColor = Color.LightSalmon,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Blue",
+                ColorName = "Blue",
+                BgColor = Color.Blue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DarkSlateBlue",
+                ColorName = "Dark Slate Blue",
+                BgColor = Color.DarkSlateBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightSlateGray",
+                ColorName = "Light Slate Gray",
+                BgColor = Color.LightSlateGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LimeGreen",
+                ColorName = "Lime Green",
+                BgColor = Color.LimeGreen,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Yellow",
+                ColorName = "Yellow",
+                BgColor = Color.Yellow,
+                FntColor = Color.Black
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DodgerBlue",
+                ColorName = "Dodger Blue",
+                BgColor = Color.DodgerBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Tomato",
+                ColorName = "Tomato",
+                BgColor = Color.Tomato,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Green",
+                ColorName = "Green",
+                BgColor = Color.Green,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Orange",
+                ColorName = "Orange",
+                BgColor = Color.Orange,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Brown",
+                ColorName = "Brown",
+                BgColor = Color.Brown,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Maroon",
+                ColorName = "Maroon",
+                BgColor = Color.Maroon,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DarkSlateGray",
+                ColorName = "Dark Slate Gray",
+                BgColor = Color.DarkSlateGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DimGray",
+                ColorName = "Dim Gray",
+                BgColor = Color.DimGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightGreen",
+                ColorName = "Light Green",
+                BgColor = Color.LightGreen,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.CornflowerBlue",
+                ColorName = "Cornflower Blue",
+                BgColor = Color.CornflowerBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightSalmon",
+                ColorName = "Light Salmon",
+                BgColor = Color.LightSalmon,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Blue",
+                ColorName = "Blue",
+                BgColor = Color.Blue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DarkSlateBlue",
+                ColorName = "Dark Slate Blue",
+                BgColor = Color.DarkSlateBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightSlateGray",
+                ColorName = "Light Slate Gray",
+                BgColor = Color.LightSlateGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LimeGreen",
+                ColorName = "Lime Green",
+                BgColor = Color.LimeGreen,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Yellow",
+                ColorName = "Yellow",
+                BgColor = Color.Yellow,
+                FntColor = Color.Black
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DodgerBlue",
+                ColorName = "Dodger Blue",
+                BgColor = Color.DodgerBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Tomato",
+                ColorName = "Tomato",
+                BgColor = Color.Tomato,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Green",
+                ColorName = "Green",
+                BgColor = Color.Green,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Orange",
+                ColorName = "Orange",
+                BgColor = Color.Orange,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Brown",
+                ColorName = "Brown",
+                BgColor = Color.Brown,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Maroon",
+                ColorName = "Maroon",
+                BgColor = Color.Maroon,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DarkSlateGray",
+                ColorName = "Dark Slate Gray",
+                BgColor = Color.DarkSlateGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DimGray",
+                ColorName = "Dim Gray",
+                BgColor = Color.DimGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightGreen",
+                ColorName = "Light Green",
+                BgColor = Color.LightGreen,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.CornflowerBlue",
+                ColorName = "Cornflower Blue",
+                BgColor = Color.CornflowerBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightSalmon",
+                ColorName = "Light Salmon",
+                BgColor = Color.LightSalmon,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Blue",
+                ColorName = "Blue",
+                BgColor = Color.Blue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DarkSlateBlue",
+                ColorName = "Dark Slate Blue",
+                BgColor = Color.DarkSlateBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightSlateGray",
+                ColorName = "Light Slate Gray",
+                BgColor = Color.LightSlateGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LimeGreen",
+                ColorName = "Lime Green",
+                BgColor = Color.LimeGreen,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Yellow",
+                ColorName = "Yellow",
+                BgColor = Color.Yellow,
+                FntColor = Color.Black
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DodgerBlue",
+                ColorName = "Dodger Blue",
+                BgColor = Color.DodgerBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Tomato",
+                ColorName = "Tomato",
+                BgColor = Color.Tomato,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Green",
+                ColorName = "Green",
+                BgColor = Color.Green,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Orange",
+                ColorName = "Orange",
+                BgColor = Color.Orange,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Brown",
+                ColorName = "Brown",
+                BgColor = Color.Brown,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Maroon",
+                ColorName = "Maroon",
+                BgColor = Color.Maroon,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DarkSlateGray",
+                ColorName = "Dark Slate Gray",
+                BgColor = Color.DarkSlateGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DimGray",
+                ColorName = "Dim Gray",
+                BgColor = Color.DimGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightGreen",
+                ColorName = "Light Green",
+                BgColor = Color.LightGreen,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.CornflowerBlue",
+                ColorName = "Cornflower Blue",
+                BgColor = Color.CornflowerBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightSalmon",
+                ColorName = "Light Salmon",
+                BgColor = Color.LightSalmon,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Blue",
+                ColorName = "Blue",
+                BgColor = Color.Blue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.DarkSlateBlue",
+                ColorName = "Dark Slate Blue",
+                BgColor = Color.DarkSlateBlue,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LightSlateGray",
+                ColorName = "Light Slate Gray",
+                BgColor = Color.LightSlateGray,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.LimeGreen",
+                ColorName = "Lime Green",
+                BgColor = Color.LimeGreen,
+                FntColor = Color.White
+            });
+
+            SubColors.Add(new Colors
+            {
+                BackGroundColor = "Color.Yellow",
+                ColorName = "Yellow",
+                BgColor = Color.Yellow,
+                FntColor = Color.Black
+            });
+
+            foreach (var subColor in SubColors)
+            {
+                ColorsObservableCollection?.Add(subColor);
+            }
+        }
+    }
+}
